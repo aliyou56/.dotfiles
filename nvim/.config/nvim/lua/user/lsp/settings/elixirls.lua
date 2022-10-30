3ad814899a1fba9bb3e0cbc1a67657e66efb201e
Aliyou Sylla <aliyou29@hotmail.com>
2022-10-30 14:49:37 +0100

    cleaning

--- nvim/.config/nvim/lua/user/lsp/settings/elixirls.lua
+++
@@ -1,8 +0,0 @@
-return {
-  default_config = {
-    filetypes = { 'elixir', 'eelixir', 'heex', 'surface' },
-    -- root_dir = function(fname)
-    --   return util.root_pattern('mix.exs', '.git')(fname) or vim.loop.os_homedir()
-    -- end,
-  },
-}
