0feb0df23eff0124bb078f1955947aefc8130d1d
Aliyou Sylla <aliyou29@hotmail.com>
2025-07-26 16:06:25 +0200

    clean fish config

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -4,16 +4,13 @@
 
 set -g fish_greeting
 set -gx EIDITOR nvim
-#set -gx ATUIN_NOBIND true
 
-# set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep
-# set -x FZF_DEFAULT_OPTS_FILE ~/.config/fzf
-# set fzf_diff_highlighter delta --paging=never --width=20
-
+fish_add_path /nix/var/nix/profiles/default/bin
 fish_add_path $HOME/.nix-profile/bin
 fish_add_path $HOME/.local/share/coursier/bin
 fish_add_path $HOME/.local/bin # set PATH so it includes user's private bin if it exists
 fish_add_path $HOME/.encore/bin
+# fish_add_path "$PATH:$HOME/.cargo/bin"
 
 if test (uname) = Darwin
   fish_add_path $HOME/Library/Application Support/Coursier/bin
@@ -40,21 +37,15 @@ end
 starship init fish | source
 zoxide init fish | source
 direnv hook fish | source
-#atuin init fish | source
 
+# to be removed
 set -gx PYENV_ROOT "$HOME/.pyenv"
 command -v pyenv >/dev/null || set -x PATH "$PATH:$PYENV_ROOT/bin"
 pyenv init - | source
 #pyenv virtualenv-init - | source
-
-# set -x PATH "$PATH:$HOME/.cargo/bin"
 
 function multicd
   echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
 end
 abbr --add dotdot --regex '^\.\.+$' --function multicd
 abbr --add fish-reload-config 'source ~/.config/fish/**/*.fish'
-
-# bind to ctrl-r in normal and insert mode, add any other bindings you want here too
-# bind \cr _atuin_search
-# bind -M insert \cr _atuin_search
