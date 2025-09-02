01f2582fa13cc8969a5bbafda805136ff583d4d2
Aliyou Sylla <aliyou29@hotmail.com>
2025-09-02 22:09:17 +0200

    (fish): fix coursier binaries path

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -7,14 +7,15 @@ set -gx EIDITOR nvim
 
 fish_add_path /nix/var/nix/profiles/default/bin
 fish_add_path $HOME/.nix-profile/bin
-fish_add_path $HOME/.local/share/coursier/bin
 fish_add_path $HOME/.local/bin # set PATH so it includes user's private bin if it exists
 fish_add_path $HOME/.encore/bin
 # fish_add_path "$PATH:$HOME/.cargo/bin"
 
 if test (uname) = Darwin
-  fish_add_path $HOME/Library/Application Support/Coursier/bin
+  fish_add_path "$HOME/Library/Application Support/Coursier/bin"
   eval (/opt/homebrew/bin/brew shellenv)
+else
+  fish_add_path $HOME/.local/share/coursier/bin
 end
 
 # source global settings
